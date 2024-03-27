5df41887d946f7f7351d3407ad824341ce3b126d
leeshin20 <leeshin20@naver.com>
2024-03-27 17:55:00 +0900

    feat: 게임 진행 클래스 구현

---
+++ src/main/java/chess/ChessGameManager.java
@@ -0,0 +1,22 @@
+package chess;
+
+import chess.ui.InputView;
+import chess.ui.OutputView;
+
+public class ChessGameManager {
+    public void startNewGame() {
+        OutputView.printStartMessage();
+        if (InputView.inputCommand().equals("start")) {
+            Board board = new Board();
+            board.initializeBoard();
+            OutputView.printBoard(board);
+            proceedGame(board);
+        }
+    }
+
+    public void proceedGame(Board board) {
+        while(!InputView.inputCommand().equals("end")) {
+
+        }
+    }
+}
