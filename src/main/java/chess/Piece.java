627701dfb0b0a9c51083657cfb41728179c8cf65
leeshin20 <leeshin20@naver.com>
2024-03-27 12:44:21 +0900

    feat: 기물 생성 위한 Piece 클래스 생성

---
+++ src/main/java/chess/Piece.java
@@ -0,0 +1,12 @@
+package chess;
+
+public class Piece {
+    private final String name;
+    private final String team;
+
+    public Piece(String name, String team) {
+        this.name = name;
+        this.team = team;
+    }
+
+}
