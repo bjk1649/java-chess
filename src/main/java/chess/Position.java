5a9cce731285468ccca69783539ff682f5702b53
leeshin20 <leeshin20@naver.com>
2024-03-27 15:42:30 +0900

    test: Position 생성 확인 테스트 코드 작성

--- src/main/java/chess/Position.java
+++ src/main/java/chess/Position.java
@@ -1,10 +1,20 @@
 package chess;
 
+import java.io.IOException;
+
 public class Position {
     private final char row;
     private final int column;
     public Position(char row, int column) {
         this.row = row;
         this.column = column;
+    }
+
+    public char getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
     }
 }
