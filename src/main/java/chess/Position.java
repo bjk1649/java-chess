04dc50daf53223de30b6b7628ee1339c4bcffde4
leeshin20 <leeshin20@naver.com>
2024-03-28 16:45:36 +0900

    feat: Map에서 Position을 키 값으로 사용하기 위한 메서드 오버라이드 추가

--- src/main/java/chess/Position.java
+++ src/main/java/chess/Position.java
@@ -1,10 +1,11 @@
 package chess;
 
-import java.io.IOException;
+import java.util.Objects;
 
 public class Position {
     private final char row;
     private final int column;
+
     public Position(char row, int column) {
         this.row = row;
         this.column = column;
@@ -16,5 +17,19 @@ public class Position {
 
     public int getColumn() {
         return column;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return row == position.row &&
+                column == position.column;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, column);
     }
 }
