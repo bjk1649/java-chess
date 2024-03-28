2fd51294043194ca43a3d9a1415fb02efa229826
leeshin20 <leeshin20@naver.com>
2024-03-28 16:46:44 +0900

    test: 새로운 기물 생성 테스트 코드 수정

--- src/test/java/chess/PieceTest.java
+++ src/test/java/chess/PieceTest.java
@@ -1,13 +1,13 @@
 package chess;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PieceTest {
     @Test
-    void createNewPiece(){
-        Piece testPiece = new Piece(Piece.Type.BISHOP,Piece.Team.WHITE);
+    void createNewPiece() {
+        Piece testPiece = new Piece(Piece.Type.BISHOP, Piece.Team.WHITE);
         assertThat(testPiece.getType()).isEqualTo(Piece.Type.BISHOP);
         assertThat(testPiece.getTeam()).isEqualTo(Piece.Team.WHITE);
     }
