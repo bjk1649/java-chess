1022f71fd443cc6f231eea166d2bfd9771a8cf03
leeshin20 <leeshin20@naver.com>
2024-03-28 16:41:49 +0900

    refactor: 체스판 기물 정보 입력 리팩토링

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -4,53 +4,46 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Board {
-    private Map<Position, Piece> board; //좌표, 기물종류
+    private final Map<Position, Piece> board; //좌표, 기물종류
 
     public Board() {
         this.board = new HashMap<>();
-        initializeBoard();
     }
 
-    private void initializeBoard() {
+    public void initializeBoard() {
         setBlackPiece();
         setWhitePiece();
     }
 
+    public Piece getPiece(Position position) {
+        return board.get(position);
+    }
+
     private void setBlackPiece() {
-        board.put(new Position('A',8), new Piece(Piece.Type.ROOK, Piece.Team.BLACK));
-        board.put(new Position('B',8), new Piece(Piece.Type.KNIGHT, Piece.Team.BLACK));
-        board.put(new Position('C',8), new Piece(Piece.Type.BISHOP, Piece.Team.BLACK));
-        board.put(new Position('D',8), new Piece(Piece.Type.QUEEN, Piece.Team.BLACK));
-        board.put(new Position('E',8), new Piece(Piece.Type.KING, Piece.Team.BLACK));
-        board.put(new Position('F',8), new Piece(Piece.Type.BISHOP, Piece.Team.BLACK));
-        board.put(new Position('G',8), new Piece(Piece.Type.KNIGHT, Piece.Team.BLACK));
-        board.put(new Position('H',8), new Piece(Piece.Type.ROOK, Piece.Team.BLACK));
-        board.put(new Position('A',7), new Piece(Piece.Type.PAWN, Piece.Team.BLACK));
-        board.put(new Position('B',7), new Piece(Piece.Type.PAWN, Piece.Team.BLACK));
-        board.put(new Position('C',7), new Piece(Piece.Type.PAWN, Piece.Team.BLACK));
-        board.put(new Position('D',7), new Piece(Piece.Type.PAWN, Piece.Team.BLACK));
-        board.put(new Position('E',7), new Piece(Piece.Type.PAWN, Piece.Team.BLACK));
-        board.put(new Position('F',7), new Piece(Piece.Type.PAWN, Piece.Team.BLACK));
-        board.put(new Position('G',7), new Piece(Piece.Type.PAWN, Piece.Team.BLACK));
-        board.put(new Position('H',7), new Piece(Piece.Type.PAWN, Piece.Team.BLACK));
+        board.put(new Position('A', 8), new Piece(Piece.Type.ROOK, Piece.Team.BLACK));
+        board.put(new Position('B', 8), new Piece(Piece.Type.KNIGHT, Piece.Team.BLACK));
+        board.put(new Position('C', 8), new Piece(Piece.Type.BISHOP, Piece.Team.BLACK));
+        board.put(new Position('D', 8), new Piece(Piece.Type.QUEEN, Piece.Team.BLACK));
+        board.put(new Position('E', 8), new Piece(Piece.Type.KING, Piece.Team.BLACK));
+        board.put(new Position('F', 8), new Piece(Piece.Type.BISHOP, Piece.Team.BLACK));
+        board.put(new Position('G', 8), new Piece(Piece.Type.KNIGHT, Piece.Team.BLACK));
+        board.put(new Position('H', 8), new Piece(Piece.Type.ROOK, Piece.Team.BLACK));
+        for(char row = 'A'; row <= 'H'; row++) {
+            board.put(new Position(row, 7), new Piece(Piece.Type.PAWN, Piece.Team.BLACK));
+        }
     }
 
     private void setWhitePiece() {
-        board.put(new Position('A',1), new Piece(Piece.Type.ROOK, Piece.Team.WHITE));
-        board.put(new Position('B',1), new Piece(Piece.Type.KNIGHT, Piece.Team.WHITE));
-        board.put(new Position('C',1), new Piece(Piece.Type.BISHOP, Piece.Team.WHITE));
-        board.put(new Position('D',1), new Piece(Piece.Type.KING, Piece.Team.WHITE));
-        board.put(new Position('E',1), new Piece(Piece.Type.QUEEN, Piece.Team.WHITE));
-        board.put(new Position('F',1), new Piece(Piece.Type.BISHOP, Piece.Team.WHITE));
-        board.put(new Position('G',1), new Piece(Piece.Type.KNIGHT, Piece.Team.WHITE));
-        board.put(new Position('H',1), new Piece(Piece.Type.ROOK, Piece.Team.WHITE));
-        board.put(new Position('A',2), new Piece(Piece.Type.PAWN, Piece.Team.WHITE));
-        board.put(new Position('A',2), new Piece(Piece.Type.PAWN, Piece.Team.WHITE));
-        board.put(new Position('A',2), new Piece(Piece.Type.PAWN, Piece.Team.WHITE));
-        board.put(new Position('A',2), new Piece(Piece.Type.PAWN, Piece.Team.WHITE));
-        board.put(new Position('A',2), new Piece(Piece.Type.PAWN, Piece.Team.WHITE));
-        board.put(new Position('A',2), new Piece(Piece.Type.PAWN, Piece.Team.WHITE));
-        board.put(new Position('A',2), new Piece(Piece.Type.PAWN, Piece.Team.WHITE));
-        board.put(new Position('A',2), new Piece(Piece.Type.PAWN, Piece.Team.WHITE));
+        board.put(new Position('A', 1), new Piece(Piece.Type.ROOK, Piece.Team.WHITE));
+        board.put(new Position('B', 1), new Piece(Piece.Type.KNIGHT, Piece.Team.WHITE));
+        board.put(new Position('C', 1), new Piece(Piece.Type.BISHOP, Piece.Team.WHITE));
+        board.put(new Position('D', 1), new Piece(Piece.Type.KING, Piece.Team.WHITE));
+        board.put(new Position('E', 1), new Piece(Piece.Type.QUEEN, Piece.Team.WHITE));
+        board.put(new Position('F', 1), new Piece(Piece.Type.BISHOP, Piece.Team.WHITE));
+        board.put(new Position('G', 1), new Piece(Piece.Type.KNIGHT, Piece.Team.WHITE));
+        board.put(new Position('H', 1), new Piece(Piece.Type.ROOK, Piece.Team.WHITE));
+        for(char row = 'A'; row <= 'H'; row++) {
+            board.put(new Position(row, 2), new Piece(Piece.Type.PAWN, Piece.Team.WHITE));
+        }
     }
 }
