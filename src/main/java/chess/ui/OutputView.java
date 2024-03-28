b4fb5ea326d3fed299d9ce16d424f09febe78adb
leeshin20 <leeshin20@naver.com>
2024-03-28 16:47:36 +0900

    feat: 초기화 된 체스판 출력 기능 구현

--- src/main/java/chess/ui/OutputView.java
+++ src/main/java/chess/ui/OutputView.java
@@ -1,6 +1,8 @@
 package chess.ui;
 
 import chess.Board;
+import chess.Piece;
+import chess.Position;
 
 public class OutputView {
 
@@ -9,12 +11,36 @@ public class OutputView {
         System.out.println("게임 시작은 start, 종료는 end 명령을 입력하세요.");
     }
 
-    public static void printEndMessage() {
-        System.out.println("end");
-    }
-
     public static void printBoard(Board board) {
-
+        for (int row = 8; row >= 1; row--) {
+            printOneRow(board, row);
+            System.out.println();
+        }
     }
 
+    private static void printOneRow(Board board, int row) {
+        for (char col = 'A'; col <= 'H'; col++) {
+            Position position = new Position(col, row);
+            Piece piece = board.getPiece(position);
+            printOnePiece(piece);
+        }
+    }
+
+    private static void printOnePiece(Piece piece) {
+        System.out.print(getPieceSymbol(piece));
+    }
+
+    private static String getPieceSymbol(Piece piece) {
+        if (piece == null) {
+            return ".";
+        }
+        return convertSymbolFormat(piece);
+    }
+
+    private static String convertSymbolFormat(Piece piece) {
+        if (piece.getTeam().equals(Piece.Team.WHITE)) {
+            return piece.getType().getSymbol().toLowerCase();
+        }
+        return piece.getType().getSymbol();
+    }
 }
