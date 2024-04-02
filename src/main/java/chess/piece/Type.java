70d3cc2c4193737946f205a6a49264f6de139d6d
leeshin20 <leeshin20@naver.com>
2024-04-02 12:33:35 +0900

    refactor: Type enum의 출력을 위한 Symbol 삭제

--- src/main/java/chess/piece/Type.java
+++ src/main/java/chess/piece/Type.java
@@ -1,10 +1,10 @@
 package chess.piece;
 
 public enum Type {
-    KING("K"),
-    QUEEN("Q"),
-    BISHOP("B"),
-    KNIGHT("N"),
-    ROOK("R"),
-    PAWN("P");
+    KING,
+    QUEEN,
+    BISHOP,
+    KNIGHT,
+    ROOK,
+    PAWN;
 }
