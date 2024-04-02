382c5a543ae89288c6ddb16e39f403717ecbf883
leeshin20 <leeshin20@naver.com>
2024-04-02 14:57:35 +0900

    refactor: 상수 위치 변경

--- src/main/java/chess/position/Position.java
+++ src/main/java/chess/position/Position.java
@@ -3,6 +3,10 @@ package chess.position;
 import java.util.Objects;
 
 public class Position {
+    public static final int FIRST_FILE = 1;
+    public static final int LAST_FILE = 8;
+    public  static final int FIRST_RANK = 1;
+    public static final int LAST_RANK = 8;
 
     private final File file;
     private final Rank rank;
