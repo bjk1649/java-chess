6191b38a5250050155aa171d79de6a89a1b3b574
leeshin20 <leeshin20@naver.com>
2024-04-02 14:42:31 +0900

    refactor: 사용자 입력 명령어 상수로 분리

--- src/main/java/chess/ChessGameManager.java
+++ src/main/java/chess/ChessGameManager.java
@@ -1,12 +1,16 @@
 package chess;
 
+import chess.board.Board;
 import chess.ui.InputView;
 import chess.ui.OutputView;
 
 public class ChessGameManager {
+    private static final String START_COMMAND = "start";
+    private static final String END_COMMAND = "end";
+
     public void startNewGame() {
         OutputView.printStartMessage();
-        if (InputView.inputCommand().equals("start")) {
+        if (InputView.inputCommand().equals(START_COMMAND)) {
             Board board = new Board();
             board.initializeBoard();
             OutputView.printBoard(board);
@@ -16,7 +20,7 @@ public class ChessGameManager {
 
 
     public void proceedGame(Board board) {
-        while (!InputView.inputCommand().equals("end")) {
+        while (!InputView.inputCommand().equals(END_COMMAND)) {
 
         }
     }
