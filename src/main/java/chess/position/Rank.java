9f3a4a2fadc31f01a6d001e2bcafed0ce88cb4d6
leeshin20 <leeshin20@naver.com>
2024-04-02 14:18:48 +0900

    refactor: Position클래스의 row, column을 분리 하여 enum으로 관리하도록 수정

---
+++ src/main/java/chess/position/Rank.java
@@ -0,0 +1,27 @@
+package chess.position;
+
+import java.util.Arrays;
+
+public enum Rank {
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8);
+
+    private final int value;
+
+    Rank(final int value) {
+        this.value = value;
+    }
+
+    public static Rank findByValue(int label) {
+        return Arrays.stream(values())
+                .filter(rank -> rank.value == label)
+                .findAny()
+                .orElse(null);
+    }
+}
