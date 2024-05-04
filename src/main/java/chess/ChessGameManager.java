708d2808d7e4d94e17a5c70686f706835cb9529c
leeshin20 <leeshin20@naver.com>
2024-05-05 04:00:38 +0900

    feat: 입력 받은 위치로 기물을 이동 기능 추가

--- src/main/java/chess/ChessGameManager.java
+++ src/main/java/chess/ChessGameManager.java
@@ -3,8 +3,7 @@ package chess;
 import chess.board.Board;
 import chess.ui.InputView;
 import chess.ui.OutputView;
-
-import java.util.ArrayList;
+import chess.position.Position;
 import java.util.List;
 
 public class ChessGameManager {
@@ -23,12 +22,26 @@ public class ChessGameManager {
     public void proceedGame(Board board) {
         String command = InputView.inputCommand();
         if (!command.equals(END_COMMAND)) {
-            InputView.extractMovePath(command);
+            moveprocess(board, InputView.extractMovePath(command));
             OutputView.printBoard(board);
             proceedGame(board);
         }
         if (command.equals(END_COMMAND)) {
             System.out.println("게임 종료");
         }
+    }
+
+    public void moveprocess(Board board, List<String> locationList) {
+        Position start = searchPosition(locationList.get(0));
+        Position target = searchPosition(locationList.get(1));
+
+        board.movePiece(start, target);
+    }
+
+    public Position searchPosition(String location) {
+        int file = location.charAt(0) - 'a' + 1;
+        int rank = location.charAt(1) - '0';
+
+        return new Position(file, rank);
     }
 }
