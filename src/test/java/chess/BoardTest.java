dd83cc5cda6fc3b326672c3493b650f1385d7b70
leeshin20 <leeshin20@naver.com>
2024-05-05 03:56:54 +0900

    test: 기물 이동 테스트 코드 작성

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,14 +1,26 @@
 package chess;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
+import chess.board.Board;
+import chess.position.Position;
 import org.junit.jupiter.api.Test;
 
 public class BoardTest {
     @Test
-    void initializeBoard() {
+    void movePiece() {
         Board testBoard = new Board();
-        testBoard.initializeBoard();
-        assertThat(testBoard).isNotNull();
+
+        Position start = new Position(1, 2);
+        Position target = new Position(1, 3);
+
+        assertNotNull(testBoard.findPiece(start));
+        assertNull(testBoard.findPiece(target));
+
+        testBoard.movePiece(start, target);
+
+        assertNull(testBoard.findPiece(start));
+        assertNotNull(testBoard.findPiece(target));
     }
 }
