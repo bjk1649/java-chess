6eba1a011c3d33ac1c71cbc10bcbc0cf7536ec1b
leeshin20 <leeshin20@naver.com>
2024-05-04 16:38:04 +0900

    feat: move 명령어 입력 구현

--- src/main/java/chess/ChessGameManager.java
+++ src/main/java/chess/ChessGameManager.java
@@ -4,6 +4,9 @@ import chess.board.Board;
 import chess.ui.InputView;
 import chess.ui.OutputView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChessGameManager {
     private static final String START_COMMAND = "start";
     private static final String END_COMMAND = "end";
@@ -18,8 +21,14 @@ public class ChessGameManager {
     }
 
     public void proceedGame(Board board) {
-        while (!InputView.inputCommand().equals(END_COMMAND)) {
-
+        String command = InputView.inputCommand();
+        if (!command.equals(END_COMMAND)) {
+            InputView.extractMovePath(command);
+            OutputView.printBoard(board);
+            proceedGame(board);
+        }
+        if (command.equals(END_COMMAND)) {
+            System.out.println("게임 종료");
         }
     }
 }
--- src/main/java/chess/ui/InputView.java
+++ src/main/java/chess/ui/InputView.java
@@ -1,6 +1,10 @@
 package chess.ui;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
@@ -11,5 +15,19 @@ public class InputView {
 
     public static String inputCommand() {
         return scanner.nextLine();
+    }
+
+    public static List<String> extractMovePath(String moveCommands) {
+        List<String> commandList = Arrays.stream(moveCommands.split(" "))
+                .collect(Collectors.toList());
+        checkMoveCommand(commandList);
+        commandList.remove(0);
+        return commandList;
+    }
+
+    public static void checkMoveCommand(List<String> commandList) {
+        if(!commandList.get(0).equals("move")) {
+            throw new IllegalArgumentException("올바른 move 명령을 입력해 주세요.");
+        }
     }
 }
