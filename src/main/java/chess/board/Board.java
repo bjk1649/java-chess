c38010f1216d2d929affea415785a524999be4f1
leeshin20 <leeshin20@naver.com>
2024-05-09 05:22:30 +0900

    refactor: 상수명 변경

--- src/main/java/chess/board/Board.java
+++ src/main/java/chess/board/Board.java
@@ -3,7 +3,6 @@ package chess.board;
 import chess.piece.*;
 import chess.position.Position;
 import chess.position.StartPiecePosition;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,13 +23,13 @@ public class Board {
         board.put(StartPiecePosition.BLACK_ROOK_LEFT.getPosition(), new Rook(Team.BLACK));
         board.put(StartPiecePosition.BLACK_KNIGHT_LEFT.getPosition(), new Knight(Team.BLACK));
         board.put(StartPiecePosition.BLACK_BISHOP_LEFT.getPosition(), new Bishop(Team.BLACK));
-        board.put(StartPiecePosition.BLACK_KING.getPosition(), new King(Team.BLACK));
         board.put(StartPiecePosition.BLACK_QUEEN.getPosition(), new Queen(Team.BLACK));
+        board.put(StartPiecePosition.BLACK_KING.getPosition(), new King(Team.BLACK));
         board.put(StartPiecePosition.BLACK_BISHOP_RIGHT.getPosition(), new Bishop(Team.BLACK));
         board.put(StartPiecePosition.BLACK_KNIGHT_RIGHT.getPosition(), new Knight(Team.BLACK));
         board.put(StartPiecePosition.BLACK_ROOK_RIGHT.getPosition(), new Rook(Team.BLACK));
         for (int file = Position.FIRST_FILE; file <= Position.LAST_FILE; file++) {
-            board.put(new Position(file, Position.BLACK_PAWN_RANK), new Pawn(Team.BLACK));
+            board.put(new Position(file, Position.BLACK_PAWN_INITIAL_RANK), new Pawn(Team.BLACK));
         }
     }
 
@@ -38,20 +37,19 @@ public class Board {
         board.put(StartPiecePosition.WHITE_ROOK_LEFT.getPosition(), new Rook(Team.WHITE));
         board.put(StartPiecePosition.WHITE_KNIGHT_LEFT.getPosition(), new Knight(Team.WHITE));
         board.put(StartPiecePosition.WHITE_BISHOP_LEFT.getPosition(), new Bishop(Team.WHITE));
-        board.put(StartPiecePosition.WHITE_KING.getPosition(), new King(Team.WHITE));
         board.put(StartPiecePosition.WHITE_QUEEN.getPosition(), new Queen(Team.WHITE));
+        board.put(StartPiecePosition.WHITE_KING.getPosition(), new King(Team.WHITE));
         board.put(StartPiecePosition.WHITE_BISHOP_RIGHT.getPosition(), new Bishop(Team.WHITE));
         board.put(StartPiecePosition.WHITE_KNIGHT_RIGHT.getPosition(), new Knight(Team.WHITE));
         board.put(StartPiecePosition.WHITE_ROOK_RIGHT.getPosition(), new Rook(Team.WHITE));
         for (int file = Position.FIRST_FILE; file <= Position.LAST_FILE; file++) {
-            board.put(new Position(file, Position.WHITE_PAWN_RANK), new Pawn(Team.WHITE));
+            board.put(new Position(file, Position.WHITE_PAWN_INITIAL_RANK), new Pawn(Team.WHITE));
         }
     }
 
     public void movePiece(Position start, Position target) {
-        //checkPath();
-        board.put(target, findPiece(start));
-        board.remove(start);
+        this.board.put(target, findPiece(start));
+        this.board.remove(start);
     }
 
     public Piece findPiece(Position position) {
