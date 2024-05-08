52da4c350bb114d80608905462a6e7d12746bda1
leeshin20 <leeshin20@naver.com>
2024-05-09 06:26:37 +0900

    feat: 폰 특수 이동에 대한 추가 예외처리

--- src/main/java/chess/piece/Piece.java
+++ src/main/java/chess/piece/Piece.java
@@ -31,4 +31,13 @@ public abstract class Piece {
             throw new IllegalStateException("같은 팀 기물이 있는 위치로 이동 할 수 없습니다.");
         }
     }
+
+    public void checkTargetPositionByPawn(Piece targetPiece, int fileGap) {
+        if (targetPiece == null && fileGap != 0) {
+            throw new IllegalArgumentException("폰은 상대 기물을 공격할 때만 대각선으로 이동할 수 있습니다.");
+        }
+        if (targetPiece != null && fileGap == 0) {
+            throw new IllegalArgumentException("폰은 정면에 있는 상대 기물을 공격할 수 없습니다.");
+        }
+    }
 }
