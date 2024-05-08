ff296ccfea05941d0980ef20928b6b99a4887754
leeshin20 <leeshin20@naver.com>
2024-05-09 06:53:45 +0900

    feat: Pawn의 현재 상태에 따른 이동 가능 범위 갱신 기능 구현

--- src/main/java/chess/piece/Pawn.java
+++ src/main/java/chess/piece/Pawn.java
@@ -1,7 +1,43 @@
 package chess.piece;
 
+import static chess.move.Movement.*;
+
+import chess.move.Movement;
+import chess.position.Position;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Pawn extends Piece {
+    private static final List<Movement> BLACK_PAWN_MOVABLE_DIRECTION = List.of(DOWN, DOWN_RIGHT, DOWN_LEFT);
+    private static final List<Movement> BLACK_PAWN_FIRST_MOVABLE_DIRECTION = List.of(DOWN, DOWN_DOWN);
+
+    private static final List<Movement> WHITE_PAWN_MOVABLE_DIRECTION = List.of(UP, UP_RIGHT, UP_LEFT);
+    private static final List<Movement> WHITE_PAWN_FIRST_MOVABLE_DIRECTION = List.of(UP, UP_UP);
+
     public Pawn(final Team team) {
         super(team);
+    }
+
+    private List<Movement> updateWhitePawnMovableDirection(Position start) {
+        List<Movement> movements = new ArrayList<>();
+
+        if (start.onInitialWhitePawnRank(this.getTeam())) {
+            movements.addAll(WHITE_PAWN_FIRST_MOVABLE_DIRECTION);
+            return movements;
+        }
+        movements.addAll(WHITE_PAWN_MOVABLE_DIRECTION);
+        return movements;
+    }
+
+    private List<Movement> updateBlackPawnMovableDirection(Position start) {
+        List<Movement> movements = new ArrayList<>();
+
+        if (start.onInitialBlackPawnRank(this.getTeam())) {
+            movements.addAll(BLACK_PAWN_FIRST_MOVABLE_DIRECTION);
+            return movements;
+        }
+        movements.addAll(BLACK_PAWN_MOVABLE_DIRECTION);
+        return movements;
     }
 }
