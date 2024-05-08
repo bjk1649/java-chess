b56ad7a1d0bc101126e11d02dc3eb991e1865494
leeshin20 <leeshin20@naver.com>
2024-05-09 05:22:30 +0900

    refactor: 상수명 변경

--- src/main/java/chess/position/Position.java
+++ src/main/java/chess/position/Position.java
@@ -7,8 +7,8 @@ public class Position {
     public static final int LAST_FILE = 8;
     public static final int FIRST_RANK = 1;
     public static final int LAST_RANK = 8;
-    public static final int BLACK_PAWN_RANK = 7;
-    public static final int WHITE_PAWN_RANK = 2;
+    public static final int BLACK_PAWN_INITIAL_RANK = 7;
+    public static final int WHITE_PAWN_INITIAL_RANK = 2;
 
     private final File file;
     private final Rank rank;
