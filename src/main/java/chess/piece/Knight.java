a0ae5c1c12e24cbe9ab737446f20a27917b36407
leeshin20 <leeshin20@naver.com>
2024-05-09 06:46:54 +0900

    feat: Knight 이동 구현

--- src/main/java/chess/piece/Knight.java
+++ src/main/java/chess/piece/Knight.java
@@ -1,7 +1,30 @@
 package chess.piece;
 
+import static chess.move.Movement.*;
+
+import chess.move.Movement;
+import chess.position.Position;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Knight extends Piece {
+    private static final List<Movement> MOVABLE_DIRECTION = List.of(
+            UP_UP_RIGHT, UP_UP_LEFT, RIGHT_RIGHT_UP, RIGHT_RIGHT_DOWN,
+            DOWN_DOWN_RIGHT, DOWN_DOWN_LEFT, LEFT_LEFT_UP, LEFT_LEFT_DOWN);
+
     public Knight(final Team team) {
         super(team);
+    }
+
+    @Override
+    public List<Position> findPath(Position start, Position target, int fileGap, int rankGap) {
+        Movement movement = Movement.findMovementDirection(fileGap, rankGap);
+
+        verifyMovement(MOVABLE_DIRECTION, movement);
+
+        List<Position> positions = new ArrayList<>();
+        Position next = start.findNextPosition(movement);
+        positions.add(next);
+        return positions;
     }
 }
