1bf75df9143304909979d528132e770109b0c0bc
leeshin20 <leeshin20@naver.com>
2024-05-09 06:46:42 +0900

    feat: King 이동 구현

--- src/main/java/chess/piece/King.java
+++ src/main/java/chess/piece/King.java
@@ -1,7 +1,28 @@
 package chess.piece;
 
+import static chess.move.Movement.*;
+
+import chess.move.Movement;
+import chess.position.Position;
+import java.util.ArrayList;
+import java.util.List;
+
 public class King extends Piece {
+    private static final List<Movement> MOVABLE_DIRECTION = List.of(UP, DOWN, RIGHT, LEFT, UP_RIGHT, UP_LEFT, DOWN_RIGHT, DOWN_LEFT);
+
     public King(final Team team) {
         super(team);
+    }
+
+    @Override
+    public List<Position> findPath(Position start, Position target, int fileGap, int rankGap) {
+        Movement movement = Movement.findMovementDirection(fileGap, rankGap);
+
+        verifyMovement(MOVABLE_DIRECTION, movement);
+
+        List<Position> positions = new ArrayList<>();
+        Position next = start.findNextPosition(movement);
+        positions.add(next);
+        return positions;
     }
 }
