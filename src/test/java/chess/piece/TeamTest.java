9e8f4f8f583f93e865232c0b967c3cf225dac100
leeshin20 <leeshin20@naver.com>
2024-05-17 23:08:41 +0900

    test: Team 테스트 코드 작성

---
+++ src/test/java/chess/piece/TeamTest.java
@@ -0,0 +1,32 @@
+package chess.piece;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import chess.piece.Knight;
+import chess.piece.Pawn;
+import chess.piece.Piece;
+import chess.piece.Team;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TeamTest {
+    @DisplayName("각기 다른 기물이 같은 팀인지 확인할 수 있다.")
+    @Test
+    void checkSameTeam() {
+        Piece piece1 = new Pawn(Team.WHITE);
+        Piece piece2 = new Knight(Team.WHITE);
+        Piece piece3 = new Pawn(Team.BLACK);
+
+        assertTrue(piece1.getTeam().isSameTeam(piece2.getTeam()));
+        assertFalse(piece1.getTeam().isSameTeam((piece3.getTeam())));
+    }
+
+    @DisplayName("현재 턴을 변경할 수 있다.")
+    @Test
+    void changeTurn() {
+        Team turn = Team.WHITE;
+        assertThat(turn.changeTurn()).isEqualTo(Team.BLACK);
+    }
+}
