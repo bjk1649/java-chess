e02f40575294c6a28c3d231c427728f20e43d542
leeshin20 <leeshin20@naver.com>
2024-05-30 22:16:19 +0900

    test: 점수 계산 테스트 코드 작성

---
+++ src/test/java/chess/piece/ScoreTest.java
@@ -0,0 +1,46 @@
+package chess.piece;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import chess.board.Board;
+import chess.position.Position;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ScoreTest {
+    @DisplayName("각 기물이 점수를 가지고 있는지 확인한다.")
+    @Test
+    void pieceScore() {
+        assertThat(Score.findScoreByPiece(new Pawn(Team.WHITE))).isEqualTo(Score.PAWN_SCORE);
+        assertThat(Score.findScoreByPiece(new Rook(Team.BLACK))).isEqualTo(Score.ROOK_SCORE);
+    }
+
+    @DisplayName("초기화된 체스판의 점수를 계산한다.")
+    @Test
+    void calculateStatusScore() {
+        final double correctScore = 38.0;
+        Board board = new Board();
+        assertThat(Score.calculateScore(board, Team.WHITE)).isEqualTo(correctScore);
+        assertThat(Score.calculateScore(board, Team.BLACK)).isEqualTo(correctScore);
+    }
+
+    @DisplayName("Pawn이 세로 줄에 두개 있을 때 점수 계산에 반영되는지 확인한다")
+    @Test
+    void calculateScoreMultiplePawnOnOneFIle() {
+        final double correctScore = 37.0;
+        Board board = new Board();
+        board.movePiece(new Position(2,2), new Position(3,3));
+        assertThat(Score.calculateScore(board, Team.WHITE)).isEqualTo(correctScore);
+    }
+
+    @DisplayName("검은색 룩(5점)이 잡히면 점수 계산에 반영되는지 확인한다.")
+    @Test
+    void calculateScoreAfterAttackPiece() {
+        final double correctScore = 33.0;
+        Board board = new Board();
+        Position whiteRookPosition = new Position(1,1);
+        Position blackRookPosition = new Position(1,8);
+        board.movePiece(whiteRookPosition, blackRookPosition);
+        assertThat(Score.calculateScore(board, Team.BLACK)).isEqualTo(correctScore);
+    }
+}
