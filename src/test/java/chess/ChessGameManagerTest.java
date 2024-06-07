0a9be9ee3f83ffef14cf5ad30e63ecdc773c6848
leeshin20 <leeshin20@naver.com>
2024-06-07 18:20:37 +0900

    test: 검은색 king이 잡혔을 때의 테스트 코드 추가

--- src/test/java/chess/ChessGameManagerTest.java
+++ src/test/java/chess/ChessGameManagerTest.java
@@ -1,12 +1,17 @@
 package chess;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import chess.board.Board;
 import chess.piece.Pawn;
 import chess.piece.Piece;
 import chess.piece.Team;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 
 public class ChessGameManagerTest {
@@ -19,5 +24,23 @@ public class ChessGameManagerTest {
         assertThatThrownBy(() -> manager.verifyTurn(selectedPiece))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageContaining("상대방 기물을 이동시킬 수 없습니다.");
+    }
+
+    @DisplayName("검은색 킹이 잡혔을 때 게임이 종료 메세지가 출력되는지 확인한다")
+    @Test
+    void blackkingDead() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        Board board = new Board();
+        ChessGameManager manager = new ChessGameManager();
+
+        manager.moveProcess(board, "move e2 e4");
+        manager.moveProcess(board, "move f7 f5");
+        manager.moveProcess(board, "move d1 h5");
+        manager.moveProcess(board, "move e7 e5");
+        manager.moveProcess(board, "move h5 e8");
+
+        String output = outContent.toString();
+        assertThat(output).contains("WHITE 팀의 승리입니다.");
     }
 }
